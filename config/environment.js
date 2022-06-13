418fc661ce998625442b6da5d4584b9b86161711
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-06-13 15:42:12 +1000

    Use pod structure for <LeftNav> component

--- config/environment.js
+++ config/environment.js
@@ -58,7 +58,8 @@ module.exports = function (env) {
   const ENV = {
     appName: PKG.name,
     modulePrefix: PKG.name,
-    podModulePrefix: `${PKG.name}/pods`,
+    // podModulePrefix: `${PKG.name}/pods`,
+    // podModulePrefix: `${PKG.name}`,
     environment: env,
     rootURL: '/',
     locationType: 'history',
