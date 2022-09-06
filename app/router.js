a62d6fc206271dd989489e1d5e80090971a743d5
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-09-06 19:56:55 +1000

    Clean up unused templates and routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,4 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {
-  this.route('member', { path: '/member/:slug' });
-});
+Router.map(function () {});
--- app/routes/member.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class MemberRoute extends Route {
-  model(params) {
-    return {
-      member: params.slug
-    };
-  }
-}
