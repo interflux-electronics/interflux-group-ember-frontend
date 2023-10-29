e2c5886ee72c85039b222cfd24a0eae5ab5bed5b
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-10-29 21:33:23 +1100

    Unbreak mobile views

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,23 @@
 import Controller from '@ember/controller';
 
-export default class IndexController extends Controller {}
+export default class IndexController extends Controller {
+  get companies() {
+    return this.model.companies || [];
+  }
+
+  get group() {
+    return this.companies.findBy('businessName', 'Interflux Group');
+  }
+
+  get managers() {
+    if (!this.group) {
+      return [];
+    }
+
+    return this.group.members.map((member) => {
+      const { title, person } = member;
+
+      return { title, person };
+    });
+  }
+}
