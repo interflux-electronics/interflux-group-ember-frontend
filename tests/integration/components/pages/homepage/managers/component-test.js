58c20b3a47374b8bb64da96010c9862899c6aa83
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-10-29 21:47:27 +1100

    Remove invalid test, fix managers tests

--- tests/integration/components/pages/homepage/managers/component-test.js
+++ tests/integration/components/pages/homepage/managers/component-test.js
@@ -2,23 +2,61 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'interflux-group/tests/helpers';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import { setBreakpoint } from 'ember-responsive/test-support';
 
 module('Integration | Component | pages/homepage/managers', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    await render(hbs`<Pages::Homepage::Managers />`);
+    setBreakpoint(['desktop', 'widescreen']);
+
+    this.set('managers', [
+      {
+        title: 'CEO',
+        person: {
+          firstName: 'Daniel',
+          lastName: 'Werkhoven'
+        }
+      },
+      {
+        title: 'COO',
+        person: {
+          firstName: 'Annick',
+          lastName: 'Peeters'
+        }
+      },
+      {
+        title: 'COO',
+        person: {
+          firstName: 'Steven',
+          lastName: 'Teliszewski'
+        }
+      },
+      {
+        title: 'CFO',
+        person: {
+          firstName: 'Katrien',
+          lastName: 'Van Hyfte'
+        }
+      }
+    ]);
+
+    await render(
+      hbs`<Pages::Homepage::Managers @managers={{this.managers}} />`
+    );
 
     const headings = this.element.querySelectorAll('li strong');
     const expected = [
       'Daniel Werkhoven',
       'Annick Peeters',
-      'Steven Teliszewski'
+      'Steven Teliszewski',
+      'Katrien Van Hyfte'
     ];
 
-    assert.strictEqual(headings.length, 3);
+    assert.strictEqual(headings.length, 4);
     assert.strictEqual(headings[0].innerText, expected[0]);
     assert.strictEqual(headings[1].innerText, expected[1]);
     assert.strictEqual(headings[2].innerText, expected[2]);
+    assert.strictEqual(headings[3].innerText, expected[3]);
   });
 });
